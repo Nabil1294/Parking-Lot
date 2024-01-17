9c83bf7e69b9527903967757997fb2d294106587
Mahmoud Nabil <mahmoudmuhamednabil@gmail.com>
2024-01-17 01:26:20 -0500

    fixing database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URI || 'mongodb://127.0.0.1:27017/myParkingLotApp');
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
 
 module.exports = mongoose.connection;
 
